b04d2210a2e0079137ccd6b928390972f3f5c802
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-10-12 14:30:54 +0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='hiphip',
+  packages=find_packages(),
+  version='0.2.0',
+  description='',
+  url='https://github.com/nikitanovosibirsk/hiphip',
+  author='Nikita Tsvetkov',
+  author_email='nikitanovosibirsk@yandex.com',
+  license='MIT',
+  install_requires=[
+    'requests'
+  ]
+)
