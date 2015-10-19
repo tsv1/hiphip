5405c914f7fbe452e0fbefa4c9c3e65886b17c16
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2015-10-19 18:45:46 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
   name='hiphip',
-  packages=find_packages(),
-  version='0.2.0',
   description='',
+  version='0.1.0',
   url='https://github.com/nikitanovosibirsk/hiphip',
   author='Nikita Tsvetkov',
   author_email='nikitanovosibirsk@yandex.com',
   license='MIT',
+  packages=find_packages(),
   install_requires=[
-    'requests'
+    'requests>=2.2.1'
   ]
 )
