52b5b00085528c0f5554b4a5ee035d0d868f5eeb
Юрий <ko10ok@users.noreply.github.com>
2018-11-21 14:33:28 +0700

    добавил indent, добавил else блок, убрал лишнюю проверку

--- hiphip/http/response.py
+++ hiphip/http/response.py
@@ -10,14 +10,16 @@ class Response(requests.Response):
         r = "{method} {url}\n".format(method=self.request.method, url=self.request.url)
 
         request_content_type = self.request.headers.get("content-type", "text/plain").lower()
-        if request_content_type.startswith("text/") or request_content_type.endswith("/text"):
+        if request_content_type.startswith("text/"):
             r += str(self.request.body)
         elif request_content_type.startswith("application/json"):
             try:
                 request_text = json.loads(self.request.body)
             except:
-                request_text = 'Raw: {}'.format(self.request.body)
-            r += json.dumps(request_text, sort_keys=True, ensure_ascii=False, indent=2)
+                request_text = 'Broken json: {}'.format(self.request.body)
+            r += json.dumps(request_text, sort_keys=True, ensure_ascii=False, indent=4)
+        else: 
+            r += str(self.request.body)
         r += "\n\n"
         
         r += "HTTP/1.1 {status}\n".format(status=self.status_code)
@@ -25,13 +27,14 @@ class Response(requests.Response):
             r += "{key}: {value}\n".format(key=key, value=value)
 
         content_type = self.headers.get("content-type", "text/plain").lower()
-        if content_type.startswith("text/") or content_type.endswith("/text"):
+        if content_type.startswith("text/"):
             r += "\n" + str(self.text)
         elif content_type.startswith("application/json"):
             try:
                 text = json.loads(self.text)
             except:
                 text = ""
-            r += "\n" + json.dumps(text, sort_keys=True, ensure_ascii=False, indent=2)
-
+            r += "\n" + json.dumps(text, sort_keys=True, ensure_ascii=False, indent=4)
+        else: 
+            r += str(self.request.body)
         return r
