081428ff02f990794d150f21c1de52eecb91f88a
Юрий <ko10ok@users.noreply.github.com>
2018-11-22 10:24:46 +0700

    вынес общую часть. поправил try-else

--- hiphip/http/response.py
+++ hiphip/http/response.py
@@ -5,36 +5,34 @@ import requests
 
 
 class Response(requests.Response):
-
     def __repr__(self):
+        def _log_resp(headers, body):
+            r = ""
+
+            request_content_type = headers.get("content-type", "text/plain").lower()
+            if request_content_type.startswith("text/"):
+                r += str(body)
+            elif request_content_type.startswith("application/json"):
+                try:
+                    request_text = json.loads(body)
+                except:
+                    r += "Non-parsed json: {}".format(str(body))
+                else:
+                    r += json.dumps(request_text, sort_keys=True, ensure_ascii=False, indent=4)
+            else:
+                r += str(body)
+
+            return r
+
         r = "{method} {url}\n".format(method=self.request.method, url=self.request.url)
 
-        request_content_type = self.request.headers.get("content-type", "text/plain").lower()
-        if request_content_type.startswith("text/"):
-            r += str(self.request.body)
-        elif request_content_type.startswith("application/json"):
-            try:
-                request_text = json.loads(self.request.body)
-            except:
-                request_text = 'Broken json: {}'.format(self.request.body)
-            r += json.dumps(request_text, sort_keys=True, ensure_ascii=False, indent=4)
-        else: 
-            r += str(self.request.body)
+        r += _log_resp(self.request.headers, self.request.body)
         r += "\n\n"
-        
+
         r += "HTTP/1.1 {status}\n".format(status=self.status_code)
         for key, value in OrderedDict(sorted(self.headers.items())).items():
             r += "{key}: {value}\n".format(key=key, value=value)
 
-        content_type = self.headers.get("content-type", "text/plain").lower()
-        if content_type.startswith("text/"):
-            r += "\n" + str(self.text)
-        elif content_type.startswith("application/json"):
-            try:
-                text = json.loads(self.text)
-            except:
-                text = ""
-            r += "\n" + json.dumps(text, sort_keys=True, ensure_ascii=False, indent=4)
-        else: 
-            r += str(self.request.body)
+        r += _log_resp(self.headers, self.text)
+
         return r
