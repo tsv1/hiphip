7ffac0e9e18368c847af873a2638ec13f4bccb28
Юрий <ko10ok@users.noreply.github.com>
2018-11-22 10:33:18 +0700

    extended request output (#1)
    * extended request output
    
    * добавил indent, добавил else блок, убрал лишнюю проверку
    
    * вынес общую часть. поправил try-else
    
    * поправил нейминг

--- hiphip/http/response.py
+++ hiphip/http/response.py
@@ -5,22 +5,34 @@ import requests
 
 
 class Response(requests.Response):
-
     def __repr__(self):
-        r = "{method} {url}\n\n".format(method=self.request.method, url=self.request.url)
+        def _log_body(headers, body):
+            r = ""
+
+            request_content_type = headers.get("content-type", "text/plain").lower()
+            if request_content_type.startswith("text/"):
+                r += str(body)
+            elif request_content_type.startswith("application/json"):
+                try:
+                    request_text = json.loads(body)
+                except:
+                    r += "Non-parsed json: {}".format(str(body))
+                else:
+                    r += json.dumps(request_text, sort_keys=True, ensure_ascii=False, indent=4)
+            else:
+                r += str(body)
+
+            return r
+
+        r = "{method} {url}\n".format(method=self.request.method, url=self.request.url)
+
+        r += _log_resp(self.request.headers, self.request.body)
+        r += "\n\n"
 
         r += "HTTP/1.1 {status}\n".format(status=self.status_code)
         for key, value in OrderedDict(sorted(self.headers.items())).items():
             r += "{key}: {value}\n".format(key=key, value=value)
 
-        content_type = self.headers.get("content-type", "text/plain").lower()
-        if content_type.startswith("text/") or content_type.endswith("/text"):
-            r += "\n" + str(self.text)
-        elif content_type.startswith("application/json"):
-            try:
-                text = json.loads(self.text)
-            except:
-                text = ""
-            r += "\n" + json.dumps(text, sort_keys=True, ensure_ascii=False, indent=2)
+        r += _log_resp(self.headers, self.text)
 
         return r
